b19a784e998a2dc0ad97323062ed3c988399483f
yoosion030 <rsy5487@naver.com>
2022-01-24 00:21:59 +0900

    Update LoginPage Design

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import * as S from './style'
+import * as I from '../../Assets'
 import GoogleLogin from 'react-google-login'
-
+import axios from 'axios'
+import api from '../../api.js'
 const LoginPage = () => {
   const navigate = useNavigate()
   const clientId =
     '625161595668-667irjaah6c338grk6pv6gjddg44n5sb.apps.googleusercontent.com'
-  const onSuccess = () => {
-    navigate('/plan')
+  const onSuccess = res => {
+    console.log(res)
+    api.post('http://louis7308.iptime.org:3001/').then(res => {
+      console.log(res)
+    })
   }
   return (
     <>
       <S.MainSection>
-        <S.Title>당신의 신년계획을 아름답게 작성해보세요!</S.Title>
-        <GoogleLogin
-          buttonText="구글 로그인"
+        <I.LoginBackground></I.LoginBackground>
+        <S.Title>Welcome to new year plan</S.Title>
+        <S.SubTitle>새해가 밝았습니다 신년계획을 작성해 보세요</S.SubTitle>
+        {/* <GoogleLogin
+          buttonText="Sign in with Google"
           accessType="offline"
           responseType="permission"
           approvalPrompt="force"
@@ -23,7 +30,10 @@ const LoginPage = () => {
           clientId={clientId}
           onSuccess={onSuccess}
           className="googleLogin"
-        ></GoogleLogin>
+        ></GoogleLogin> */}
+        <a href="http://louis7308.iptime.org:3001/">
+          <button onClick={onSuccess}></button>
+        </a>
       </S.MainSection>
     </>
   )
--- src/Components/Login/style.js
+++ src/Components/Login/style.js
@@ -1,17 +1,39 @@
 import styled from 'styled-components'
 export const MainSection = styled.div`
+  overflow: hidden;
   height: 100vh;
-  background-color: #f6d284;
-  box-sizing: border-box;
-  padding: 500px 0 0 200px;
-  .googleLogin {
+  background-color: #000000;
+  svg {
+    position: absolute;
+  }
+  button {
+    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 5px;
     width: 460px;
     height: 80px;
-    cursor: pointer;
+    font-family: 'KOTRA' !important;
+    color: #000 !important;
+    font-size: 18px !important;
+    margin: 0 auto;
+  }
+  * {
+    position: relative;
+    z-index: 2;
+    text-align: center;
   }
 `
 
 export const Title = styled.div`
   font-size: 36px;
   margin-bottom: 14px;
+  font-family: 'Malgun Gothic';
+  font-size: 70px;
+  margin: 150px 0 23px;
+  font-weight: bold;
+`
+export const SubTitle = styled.div`
+  color: #ffffff;
+  font-family: 'Malgun Gothic';
+  font-size: 24px;
+  margin-bottom: 345px;
 `
