ec52f957f649df39f108952825f603d56c7e27a0
yoosion030 <rsy5487@naver.com>
2022-01-24 16:53:28 +0900

    Add userData

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import * as S from './style'
 import * as I from '../../Assets'
+import { useRecoilState } from 'recoil'
+import { Profile } from '../../Atom'
 
 const Header = () => {
+  const [userProfile, setProfile] = useRecoilState(Profile)
   return (
     <S.Header>
       <S.Container>
@@ -21,12 +24,15 @@ const Header = () => {
           to="/share"
           style={({ isActive }) => ({
             borderBottom: isActive ? '5px solid #88400C' : 'none',
+            borderRadius: isActive ? '5px' : 'none',
           })}
         >
           남 계획
         </NavLink>
       </S.Container>
-      <S.Profile></S.Profile>
+      <Link to="/user">
+        <img src={userProfile} alt=""></img>
+      </Link>
     </S.Header>
   )
 }
