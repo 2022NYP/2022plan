9fa44595435ed6707010a367128b56038f5fbb90
yoosion030 <rsy5487@naver.com>
2022-01-28 23:16:36 +0900

    Update name

--- src/Components/Plan/PlanItem/index.js
+++ src/Components/Plan/PlanItem/index.js
@@ -1,23 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
-import { plan } from '../../../Atom'
+import { useRecoilState } from 'recoil'
+import { saved } from '../../../Atom'
 import * as S from './style'
 
 const PlanItem = props => {
   const [title, setTitle] = useState('제목입력')
   const [content, setContent] = useState('내용입력')
   const [data, setData] = useState({ title: '', content: '' })
-  const [test, setTest] = useRecoilState(plan)
+  const [save, setSave] = useRecoilState(saved)
 
   useEffect(() => {
     setData({ title: title, content: content, id: props.i })
-    // console.log(data)
   }, [title, content])
 
-  const save = props => {
-    console.log(data)
-    console.log(test[props.i])
-    console.log(data.id)
+  const TrySave = () => {
+    setSave(save.concat(data))
+    console.log(save)
   }
   return (
     <>
@@ -36,7 +34,7 @@ const PlanItem = props => {
             onChange={e => setContent(e.target.value)}
           ></textarea>
         </S.Content>
-        <button onClick={save}>저장</button>
+        <button onClick={TrySave}>저장</button>
       </S.MainSection>
     </>
   )
--- src/Components/Plan/index.js
+++ src/Components/Plan/index.js
@@ -2,25 +2,25 @@ import React, { useState } from 'react'
 import * as S from './style'
 import PlanItem from './PlanItem'
 import * as I from '../../Assets'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { useRecoilState } from 'recoil'
 import { userName, plan } from '../../Atom'
 
 const PlanPage = () => {
   const [name, setName] = useRecoilState(userName)
-  const plans = useRecoilValue(plan)
-  const [test, setTest] = useRecoilState(plan)
-  console.log(plans)
+  const [plans, setPlans] = useRecoilState(plan)
   return (
     <>
       <S.MainSection>
         <S.Title>{name}님의 신년계획</S.Title>
         <S.PlanSection>
-          {test.map((ele, i) => (
+          {plans.map((ele, i) => (
             <PlanItem i={i}></PlanItem>
           ))}
           <S.PlusBtn
             onClick={() => {
-              setTest(test.concat({ title: '제목 입력', content: '내용 입력' }))
+              setPlans(
+                plans.concat({ title: '제목 입력', content: '내용 입력' })
+              )
             }}
           >
             <I.Plus></I.Plus>
