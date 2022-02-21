ce783af9eeef3b71d566bc6a5aafe7cdc924c8af
yoosion030 <rsy5487@naver.com>
2022-02-21 17:46:11 +0900

    Update SVG

--- src/Assets/Plus.js
+++ src/Assets/Plus.js
@@ -1,24 +1,18 @@
-import React from 'react'
+const Plus = () => (
+  <svg
+    width="46"
+    height="46"
+    viewBox="0 0 46 46"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M23 43V3M3 23H43"
+      stroke="black"
+      strokeWidth="5"
+      strokeLinecap="round"
+    />
+  </svg>
+);
 
-const Plus = () => {
-  return (
-    <>
-      <svg
-        width="46"
-        height="46"
-        viewBox="0 0 46 46"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M23 43V3M3 23H43"
-          stroke="black"
-          strokeWidth="5"
-          strokeLinecap="round"
-        />
-      </svg>
-    </>
-  )
-}
-
-export default Plus
+export default Plus;
--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,2 +1,3 @@
-export { default as Logo } from './Logo'
-export { default as Plus } from './Plus'
+export { default as Logo } from "./Logo";
+export { default as Plus } from "./Plus";
+export { default as NotFoundImg } from "./NotFoundImg";
